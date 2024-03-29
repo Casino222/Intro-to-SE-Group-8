432284024506746d37db5107b52159e55da94176
Guled Liban <GuledKING2@gmail.com>
2024-03-29 18:36:26 -0500

    what have so far
    not much but ermmm yeah

---
+++ project/script.js
@@ -0,0 +1,8 @@
+function display() {
+    // Get the value from the input tag
+    var userInput = document.getElementById("uname").value;
+
+    // Use the retrieved value
+    alert(userInput);
+    // You can use this value in any way you want, such as displaying it on the webpage, processing it, etc.
+}
